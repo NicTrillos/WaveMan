bc60913e67a10bb793d64048f9d0a0da36be4543
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2017-01-20 22:17:51 -0500

    PlayerHider in multiplayer

---
+++ Assets/Scripts/PlayerHider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerHider : NetworkBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer spriteRender;
+
+    private void Reset()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            spriteRender.enabled = false;
+        }
+    }
+}
