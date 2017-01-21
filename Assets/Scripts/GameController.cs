6df6dcea62cb035a2bc0f2226ece5d870009cb65
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2017-01-21 18:51:58 -0500

    Muros y estrellas

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,19 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
-public class GameController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private BoardManager board;
+    
+    private void Start()
+    {
+        board.SetupScene(1);
+    }
 
     public void RestartScene()
     {
