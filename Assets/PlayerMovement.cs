9c3908ac30240eaf62a6f1a1ad29ac84a571f4c6
NicTrillos <nic_trillos@hotmail.com>
2017-01-20 21:42:22 -0500

    Merge fix

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    [SerializeField]
+    private float speed = 3f;
+
+    [SerializeField]
+    private Rigidbody2D rigidbody2d;
+
+    private void Reset()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isLocalPlayer) return;
+
+        var moveX = Input.GetAxisRaw("Horizontal");
+        var moveY = Input.GetAxisRaw("Vertical");
+        
+        rigidbody2d.velocity = new Vector2(moveX, moveY) * speed;
+    }
+
+}
