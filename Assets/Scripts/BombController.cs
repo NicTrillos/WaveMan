9acc1d71754620d202fcad640efce4f248702769
NicTrillos <nic_trillos@hotmail.com>
2017-01-20 22:24:45 -0500

    Modified bomb prefab

---
+++ Assets/Scripts/BombController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour {
+
+	public float timeToExplode;
+    public bool hasExploded;
+    public ParticleSystem bombEffect;
+
+    private float currentTimeToExplode;    
+    
+    // Use this for initialization
+	void Start () {
+        currentTimeToExplode = timeToExplode;
+        hasExploded = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTimeToExplode -= Time.deltaTime;
+        if(currentTimeToExplode <= 0.0f && !hasExploded)
+        {
+            hasExploded = true;
+            GetComponent<SpriteRenderer>().enabled = false;
+            bombEffect.Play();
+        }
+	}
+}
