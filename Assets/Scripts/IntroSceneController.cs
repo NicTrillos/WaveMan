dfa8c4b43138edb255ebefc104ee47d821424c77
NicTrillos <nic_trillos@hotmail.com>
2017-01-21 22:30:23 -0500

    IntroScene

---
+++ Assets/Scripts/IntroSceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroSceneController : MonoBehaviour {
+
+    public Animator creditsAnimator;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void ShowCredits()
+    {
+        creditsAnimator.SetBool("Show", true);
+    }
+
+    public void HideCredits()
+    {
+        creditsAnimator.SetBool("Show", false);
+    }
+}
