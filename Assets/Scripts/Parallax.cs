492698b5c13a2746faa59427d9156c2c4715020c
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2017-01-21 21:12:54 -0500

    Background Stars

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Parallax : NetworkBehaviour
+{
+    [SerializeField]
+    private float speed = .4f;
+	private void Update ()
+    {
+        if (!isLocalPlayer) return;
+
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            var moveX = -Input.GetAxisRaw("Horizontal");
+            var moveY = -Input.GetAxisRaw("Vertical");
+
+            transform.Translate(moveX * speed * Time.deltaTime, moveY * speed * Time.deltaTime, 0f);
+        }
+	}
+}
